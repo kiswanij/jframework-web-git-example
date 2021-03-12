c1a62eee534cc33837cdd70e165537f64499d08e
Jalal Kiswani <kiswanij@gmail.com>
2021-03-12 13:28:38 +0200

    .

---
+++ src/main/java/com/app/person/DataAccess.java
@@ -0,0 +1,91 @@
+package com.app.person;
+
+import java.util.*;
+
+import org.eclipse.jgit.api.Git;
+
+import com.jk.util.JKIOUtil;
+import com.jk.util.JKObjectUtil;
+import com.jk.util.activedirectory.JKGitWrapper;
+import com.jk.util.config.JKConfig;
+
+public class DataAccess {
+	private static final String ID_FIELD_NAME = "id";
+	private static JKGitWrapper gw = new JKGitWrapper();
+	
+	private static Git git= gw.password(JKConfig.getDefaultInstance().getProperty("git-password2")).cloneRepo();
+
+	/////////////////////////////////////////
+	public <T> void insert(T record) {
+		List<T> list = getList((Class<T>) record.getClass());
+		list.add(record);
+		save((Class<T>)record.getClass(), list);
+	}
+
+	/////////////////////////////////////////
+	public <T> T find(Class<T> clas, Object id) {
+		List<T> list = getList(clas);
+		for (T t : list) {
+			if (JKObjectUtil.getFieldValue(t, ID_FIELD_NAME).equals(id)) {
+				return t;
+			}
+		}
+		return null;
+	}
+
+	/////////////////////////////////////////
+	public <T> void update(T record) {
+		List<T> list = (List<T>) getList(record.getClass());
+		for (int i=0;i<list.size();i++) {
+			T t = list.get(i);
+			Object sourceId = JKObjectUtil.getFieldValue(record, ID_FIELD_NAME);
+			Object currentId = JKObjectUtil.getFieldValue(t, ID_FIELD_NAME);
+			if (currentId.equals(sourceId)) {
+				list.set(i, record);
+				save((Class<T>)record.getClass(), list);
+				return;
+			}
+		}
+	}
+	
+	/////////////////////////////////////////
+	public <T> void delete(T record) {
+		List<T> list = (List<T>) getList(record.getClass());
+		for (int i=0;i<list.size();i++) {
+			T t = list.get(i);
+			Object sourceId = JKObjectUtil.getFieldValue(record, ID_FIELD_NAME);
+			Object currentId = JKObjectUtil.getFieldValue(t, ID_FIELD_NAME);
+			if (currentId.equals(sourceId)) {
+				list.remove(i);
+				save((Class<T>)record.getClass(), list);
+				return;
+			}
+		}
+	}
+
+	/////////////////////////////////////////
+	public <T> List<T> getList(Class<T> clas) {
+		String filePath = getFilePath(clas);
+		String fileContents = JKIOUtil.readFile(filePath);
+		if(fileContents==null) {
+			return new Vector<>();
+		}
+		List<T> all = (List<T>) JKObjectUtil.jsonToObjectList(fileContents, clas);
+		return all;
+	}
+
+	/////////////////////////////////////////
+	protected <T> void save(Class<T> clas, List<T> list) {
+		String contents = JKObjectUtil.toJson(list);
+		JKIOUtil.writeDataToFile(contents, getFilePath(clas));
+		gw.addCommitPush(git);
+	}
+
+	/////////////////////////////////////////
+	protected <T> String getFilePath(Class<T> clas) {
+		String fileName = clas.getName() + ".json";
+		String filePath = gw.getLocalPath() + "/" + fileName;
+		return filePath;
+	}
+
+}
